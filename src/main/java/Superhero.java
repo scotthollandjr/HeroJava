fa0515bf38440fe0989a1abd470f8c4f99f72e41
Nick Embrey and Scott Holland <scotthollandjr@gmail.com>
2016-04-27 12:04:14 -0700

    Add passing name/age test

--- src/main/java/Superhero.java
+++ src/main/java/Superhero.java
@@ -1,5 +1,17 @@
 public class Superhero {
+  private String mName;
+  private int mAge;
 
-  
+  public Superhero(String name, int age) {
+    mName = name;
+    mAge = age;
+  }
 
+  public String getName() {
+    return mName;
+  }
+
+  public int getAge() {
+    return mAge;
+  }
 }
--- src/test/java/SuperheroTest.java
+++ src/test/java/SuperheroTest.java
@@ -4,12 +4,18 @@ import java.util.ArrayList;
 
 public class SuperheroTest {
 
-  @Test
-  public void SuperheroInstantiatesCorrectly_true() {
-    Superhero batman = new Superhero();
+  @Test public void SuperheroInstantiatesCorrectly_true() {
+    Superhero batman = new Superhero("Batman", 40);
     assertEquals(true, batman instanceof Superhero);
   }
 
+  @Test public void superheroAddsName_String() {
+    Superhero batman = new Superhero("Batman", 40);
+    assertEquals("Batman", batman.getName());
+  }
 
-
+  @Test public void superHeroAddsAge_int() {
+    Superhero drGradle = new Superhero("drGradle", 999);
+    assertEquals(999, drGradle.getAge());
+  }
 }
